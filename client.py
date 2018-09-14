c15f5c7398bd0cb7294413b8f041c6f28a9a6c65
KevinTa <takevin70@gmail.com>
2018-09-13 19:51:38 -0700

    Client stays open
    Client now continues to run after run() function is complete

--- client.py
+++ client.py
@@ -1,9 +1,12 @@
 from __future__ import print_function
 
 import grpc
+import time
 
 import calculator_pb2
 import calculator_pb2_grpc
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
@@ -11,7 +14,8 @@ def run():
         number = calculator_pb2.Number(value1=99, value2=88)
         response = stub.Add(number)
     print(str(response.value1) + ' + ' + str(response.value2) + " = " + str(response.result))
-
+    while True:
+        time.sleep(_ONE_DAY_IN_SECONDS)
 
 if __name__ == '__main__':
     run()
